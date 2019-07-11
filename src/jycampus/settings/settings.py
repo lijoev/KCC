d8a0b54500b8282410e63a3436a2a81699e01f10
Lijo Varghese <lijo.varghese@seqato.com>
2019-07-11 11:09:00 +0530

    added mysql db

--- src/jycampus/settings/settings.py
+++ src/jycampus/settings/settings.py
@@ -96,10 +96,18 @@ WSGI_APPLICATION = 'jycampus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/etc/mysql/my.cnf',
+        },
     }
 }
 
